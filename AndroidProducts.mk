dca5b955cfa9e5dd6b956792d741533113b9dd24
enesuzun2002 <4lukankalar@gmail.com>
2018-02-17 19:54:08 +0000

    Add Aosp Extended Support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zeroltexx.mk \
     $(LOCAL_DIR)/lineage.mk \
+    $(LOCAL_DIR)/aosp_zeroltexx \
     $(LOCAL_DIR)/aokp.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,4 @@
 #
 
 add_lunch_combo aokp_zeroltexx-userdebug
+add_lunch_combo aosp_zeroltexx-userdebug
