a3d0178889264130d966d818fb1c035a5a360d0d
enesuzun2002 <4lukankalar@gmail.com>
2018-04-18 19:44:37 +0000

    Add asserts for T/W8
    Change-Id: Ie9f31c286efa6f9067c520a08fbe2b4511f4dff7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zeroltexx,zerolte,zerolteskt
+TARGET_OTA_ASSERT_DEVICE := zeroltexx,zeroltecan,zerolte,zerolteskt
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zeroltexx/include
