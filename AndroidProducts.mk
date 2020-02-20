d32bd0901f5f7606ba133147077ed6487dad0ecc
enesuzun2002 <enesuzun200227@gmail.com>
2020-02-20 11:58:31 +0000

    zeroltexx: switch to COMMON_LUNCH_CHOICES for Q
    Change-Id: If72eecb8789e300d2705b78d4f619a035aff42e9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zeroltexx.mk \
-    $(LOCAL_DIR)/lineage_zeroltexx.mk \
-    $(LOCAL_DIR)/viper_zeroltexx.mk \
-    $(LOCAL_DIR)/aosp_zeroltexx.mk
+    $(LOCAL_DIR)/lineage_zeroltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zeroltexx-userdebug
